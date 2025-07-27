9edb1fdffaf334df162b3b81064780140b150e0f
felipebel25 <felipemedina.developer@gmail.com>
2025-07-26 23:21:23 -0500

    useEffect hook

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import { HookUseState } from "./components/HookUseState";
+import { HookUseEffect } from "./components/HookUseEffect";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <HookUseState />
+      <HookUseEffect />
     </>
   );
 }
---
+++ src/components/HookUseEffect.tsx
@@ -0,0 +1,59 @@
+/*
+ *
+ * useEffect is the second most used hook
+ *
+ */
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+export const HookUseEffect = () => {
+  const [counter, setCounter] = useState(0);
+  const [pokemons, setPokemons] = useState([]);
+  const handleCounter = () => setCounter(counter + 1);
+
+  // if we use the component like this it will trigger the function every render
+  // the cases for useEffects may be:  Fetch API, LocalStorage
+  //
+  useEffect(() => {
+    console.log("useEffect without second params");
+  });
+  // 👉 “Run this effect only once, after the component is first mounted (i.e., after the initial render). equivalent to use componentDidMount in class functions”
+  useEffect(() => {
+    console.log("Mounting!");
+    // 👉 “Run this effect only once, after the component is unmounting
+    return () => {
+      console.log("Unmounting!");
+    };
+  }, []);
+  // fetching Data using axios
+  //   useEffect(() => {
+  //     const fetchPokeData = async () => {
+  //       const { data } = await axios(
+  //         "https://pokeapi.co/api/v2/pokemon?limit=10&offset=0"
+  //       );
+  //       setPokemons(data.results.map((pokemon) => pokemon.name));
+  //     };
+  //     fetchPokeData();
+  //   }, []);
+
+  // using fetch native API
+  useEffect(() => {
+    const fetchPokeData = async () => {
+      const data = await fetch(
+        "https://pokeapi.co/api/v2/pokemon?limit=10&offset=0"
+      );
+      const response = await data.json();
+
+      setPokemons(response.results.map((pokemon) => pokemon.name));
+    };
+    fetchPokeData();
+    return () => setPokemons([]);
+  }, []);
+  return (
+    <div>
+      {pokemons.map((pokemon) => (
+        <p key={pokemon}>{pokemon}</p>
+      ))}
+    </div>
+  );
+};
